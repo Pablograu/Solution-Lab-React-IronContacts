494186535406f20a0761e650826518fe6334de24
Pablograu <paugf89@gmail.com>
2019-12-03 16:10:11 +0100

    desonctructed props

--- starter-code/src/components/ContactCard.js
+++ starter-code/src/components/ContactCard.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default function ContactCard(props) {
+
+  const {contact: {name, pictureUrl, popularity}, index} = props
+
   return (
     <article className="contactCard">
-      <h3>{props.contact.name}</h3>
-      <img src={props.contact.pictureUrl} alt="contact img"/>
-      <h4>Popularity: {props.contact.popularity}</h4>
-      <button className="btn" onClick={()=>props.delete(props.index)}>Delete</button>
+      <h3>{name}</h3>
+      <img src={pictureUrl} alt="contact img"/>
+      <h4>Popularity: {popularity}</h4>
+      <button className="btn" onClick={()=>props.delete(index)}>Delete</button>
     </article>
   );
 }
